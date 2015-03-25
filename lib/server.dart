6ebdb5db60e5b4ff006a4a96a1aa7cb1c95d1de5
Robert Beyer <r.beyer@iplabs.de>
2015-03-25 11:09:54 +0100

    move younow package to lib folder; fix refreshTime bug

--- lib/server.dart
+++ lib/server.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:jaded/jaded.dart' as jade;
-import 'package:younow/younow.dart';
+import 'younow/younow.dart';
 
 part 'core/RouteController.dart';
 part 'core/RouteProvider.dart';
@@ -21,7 +21,6 @@ part 'core/controller/ControllerEmpty.dart';
 main() {
     Younow yn = new Younow();
     Map data;
-    int refreshTime = 30000;
 
     HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 4040).then((HttpServer server) {
         print('listening on localhost, port ${server.port}');
@@ -44,7 +43,7 @@ main() {
             ..start();
 
         //start getting younow data per interval 
-        new Timer.periodic(const Duration(milliseconds: refreshTime), (dynamic) {
+        new Timer.periodic(const Duration(milliseconds: 30000), (dynamic) {
             yn.refreshData();
             data = yn.getData();
         });
---
+++ lib/younow/younow.dart
@@ -0,0 +1,71 @@
+library younow;
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+class HttpCaller {
+    void _call(url, callback) {
+        HttpClient client = new HttpClient();
+        String jsonContent = "";
+        client.getUrl(Uri.parse(url))
+        .then((HttpClientRequest request) => request.close())
+        .then((HttpClientResponse response) {
+            response.transform(UTF8.decoder).listen((contents) {
+                jsonContent += contents.toString();
+            }, onDone:() => callback(jsonContent));
+        });
+    }
+}
+
+class YounowLocale {
+    static String LOCALE_DE = "de"; //german [de]
+    static String LOCALE_EN = "en"; //english
+    static String LOCALE_ES = "es"; //spanish [es,ca]
+    static String LOCALE_ME = "me"; //arabic [ar]
+    static String LOCALE_TR = "tr"; //turkish [tr, az, kk]
+    static String LOCALE_WW = "ww"; //worldwide
+}
+
+class Younow extends HttpCaller {
+/*
+    http://www.younow.com/php/api/younow/featured/locale=de/tag=deutsch
+    http://cdn2.younow.com/php/api/younow/dashboard
+*/
+    String dashboardUrl = "http://cdn2.younow.com/php/api/younow/dashboard";
+    Map data;
+
+    Younow() {
+        this.refreshData();
+    }
+
+    void dashboard(trendingCount, locale, callback){
+        String url = this.dashboardUrl;
+        if(trendingCount != null){
+            url += "/trending="+trendingCount;
+        }
+        if(locale != null){
+            url += "/locale="+locale;
+        }
+        print(url);
+        this._call(url, (String content) {
+            Map data = JSON.decode(content);
+            callback(data);
+        });
+    }
+
+    void refreshData() {
+        String trendingCount = "50";
+        String locale = YounowLocale.LOCALE_DE;
+
+        this.dashboard(trendingCount,locale,(jsonData){
+            this.data = jsonData;
+            print("fetched new younow data");
+        });
+    }
+
+    Map getData() {
+        return this.data;
+    }
+
+}
